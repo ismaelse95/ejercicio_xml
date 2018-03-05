2f0b1631b0c251898dd163bc9f6bb918a763aa36
Ismael Santiago <ismaelse95@gmail.com>
2018-03-05 09:44:42 +0100

    Fichero modificado

--- pubg.py
+++ pubg.py
@@ -3,8 +3,23 @@ from lxml import etree
 
 arbol=etree.parse('pubg.xml')
 
+
 #Ejercicio 1
 print("Teniendo en cuenta que el juego tiene las siguientes armas, dime una para saber el tamaño del cargador.")
-for elem in arbol:
-	print(arbol.xpath("//arma/text()"))
+print("Pistolas: ")
+print(arbol.xpath("//armas/pistolas/nombre/text()"))
+print("Escopetas: ")
+print(arbol.xpath("//armas/escopetas/nombre/text()"))
+print("Ametralladoras: ")
+print(arbol.xpath("//armas/ametralladoras/nombre/text()"))
+print("Arcos: ")
+print(arbol.xpath("//armas/arcos/nombre/text()"))
+print("Francotirador: ")
+print(arbol.xpath("//armas/francotirador/nombre/text()"))
+print("Francotirador automatico: ")
+print(arbol.xpath("//armas/francotirador_auto/nombre/text()"))
+print("Subfusiles: ")
+print(arbol.xpath("//armas/subfusiles/nombre/text()"))
+print("Rifle de asalto: ")
+print(arbol.xpath("//armas/rifle_asalto/nombre/text()"))
 arma=input("Nombre del arma: ")
