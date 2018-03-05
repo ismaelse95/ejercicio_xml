83af3408c993d519d2defbc7dec1cdd34ebe8dc0
Ismael Santiago <ismaelse95@gmail.com>
2018-03-05 09:21:02 +0100

    Fichero modificado

--- funciones_xml.py
+++ funciones_xml.py
@@ -1,9 +1,15 @@
-def lista
+#Ejercicio listar munición del arma
+def lista(texto, arma):
+	armas=texto.xpath("//nombre/text()")
+	for elem in armas:
+		if elem==arma:
+			cargador=texto.xpath("//cargador/text()")
+			return cargador
 
-def contar
+#def contar
 
-def busc_filt
+#def busc_filt
 
-def busc_info
+#def busc_info
 
-def libre
+#def libre
